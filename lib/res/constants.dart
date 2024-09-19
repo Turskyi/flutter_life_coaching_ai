2f4f206dd5db762213d239acb213e1fbe71ab6c2
Dmytro Turskyi <dmytro@turskyi.com>
2024-09-18 21:04:22 -0400

    :sparkles: Add privacy policy page - Created a new file for the Privacy Policy Page in the UI folder. - Added a link in the App Drawer to navigate to the Privacy Policy Page. - Implemented the layout and content for the Privacy Policy Page. - Included information about data collection, storage, sharing, user rights, and contact details. - Added tap gestures to open external links. - Provided options for users to delete their data and contact support. - Updated app version to 1.0.3+3 in pubspec.yaml. - Updated company domain to 'turskyi.com' in constants.dart.
    Signed-off-by: Dmytro Turskyi <dmytro.turskyi@gmail.com>

--- lib/res/constants.dart
+++ lib/res/constants.dart
@@ -1,6 +1,13 @@
 const String baseUrl = 'https://lifecoach.turskyi.com/api/';
 const String supportEmail = 'support@turskyi.com';
+const String companyDomain = 'turskyi.com';
 const String appName = 'Life-Coaching AI';
+const String authServiceLink = 'https://clerk.com';
+const String authServiceName = 'Clerk';
+const String remoteDbServiceName = 'MongoDB';
+const String remoteDbServiceLink = 'https://www.mongodb.com';
+const String deletionInstructionsLink =
+    'https://lifecoach.turskyi.com/instruction';
 const int emailMaxLength = 40;
 const int emailMinLength = 9;
 const int maxRetries = 1;
--- lib/ui/goals/app_drawer.dart
+++ lib/ui/goals/app_drawer.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lifecoach/application_services/blocs/authentication/bloc/authentication_bloc.dart';
+import 'package:lifecoach/ui/privacy/privacy_policy_page.dart';
 
 class AppDrawer extends StatelessWidget {
-  const AppDrawer({
-    super.key,
-  });
+  const AppDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +15,15 @@ class AppDrawer extends StatelessWidget {
           const DrawerHeader(
             decoration: BoxDecoration(),
             child: Text('Menu'),
+          ),
+          ListTile(
+            leading: const Icon(Icons.privacy_tip),
+            title: const Text('Privacy Policy'),
+            onTap: () => Navigator.of(context).push(
+              MaterialPageRoute<void>(
+                builder: (BuildContext context) => const PrivacyPolicyPage(),
+              ),
+            ),
           ),
           ListTile(
             leading: const Icon(Icons.logout),
